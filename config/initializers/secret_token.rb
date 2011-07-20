a945ec9a12c8e045d42d4e6bd1df0524a7b672cc
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-07-20 20:43:15 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanatorium::Application.config.secret_token = 'ce2e91165805d0e70100613ecc51c26e78366ebac484f1050f076b52a1e9092028d3f2f1790b7a6c7a103f6ca2b3985c6690b10ffb2ef58a6277b8ce42ca780f'
