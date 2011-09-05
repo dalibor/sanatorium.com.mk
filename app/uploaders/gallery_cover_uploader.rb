cbe0e37019ae176a0d26c12139b30c562107dd73
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-09-05 18:59:39 +0200

    Implemented styles for gallery index page

--- app/uploaders/gallery_cover_uploader.rb
+++ app/uploaders/gallery_cover_uploader.rb
@@ -31,7 +31,7 @@ class GalleryCoverUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [60, 60]
+    process :resize_to_fill => [200, 120]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
