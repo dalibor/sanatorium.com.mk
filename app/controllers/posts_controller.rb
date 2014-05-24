9b91f68b5399a6292b883cba1b3a78be2756ef2f
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2014-05-24 22:08:01 +0200

    Fix indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    @posts          = Post.order('published_at DESC').
-                            where('published_at IS NOT NULL').
-                            paginate(:per_page => 10, :page => params[:page])
+    @posts = Post.order('published_at DESC').
+               where('published_at IS NOT NULL').
+               paginate(:per_page => 10, :page => params[:page])
   end
 
   def show
-    @post           = Post.where('published_at IS NOT NULL').find(params[:id])
-    @comments       = @post.comments.where({:approved => true})
-    @comment        = Comment.new
+    @post     = Post.where('published_at IS NOT NULL').find(params[:id])
+    @comments = @post.comments.where({:approved => true})
+    @comment  = Comment.new
   end
 end
